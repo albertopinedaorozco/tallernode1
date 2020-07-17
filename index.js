2568637955f9545d687c2154fb0420a636262233
Fireman Pineda Orozco <ing.firemann@gmail.com>
2020-07-16 23:55:46 -0500

    Se agregan nuevas urls y se realizan validaciones

--- index.js
+++ index.js
@@ -20,21 +20,12 @@ const logger = (req, res,next) =>{
     let formatted_date = current_datetime.getFullYear() + "/" + (current_datetime.getMonth() + 1) + "/" + current_datetime.getDay()
     console.log(`${formatted_date} : ${req.method} : ${req.path} : ${req.headers['user-agent']} `);
     console.log(req.body)
-    
-
-
-    
+       
     next();
 }
 
 app.use(logger);
 app.use(express.json());
-
-/* requests */
-app.get('/', (req, res)=>{
-
-    res.status(200).send("Todo bien!");
-})
 
 app.post('/users/', (req, res)=>{
 
@@ -47,7 +38,16 @@ app.post('/users/', (req, res)=>{
         )
         return res.status(401).send("Existen  datos obligatorios que no fueron enviados.");
     
-    
+        console.log(req.body.gender)
+        if (req.body.gender != 'F' && req.body.gender != 'M')
+            return res.status(401).send("Genero incorrecto, debe ser 'F' o 'M'.");
+        
+        if (typeof(parseFloat(req.body.height))!="number")
+            return res.status(401).send("Valor incorrecto para height, debe ser numerico");
+        
+        if (typeof(parseFloat(req.body.weight))!="number") 
+          return res.status(401).send("Valor incorrecto para weight, debe ser numerico");
+
    const user = {
         identification : req.body.identification,
         name: req.body.name,
@@ -56,8 +56,8 @@ app.post('/users/', (req, res)=>{
         gender: req.body.gender,
         height: req.body.height,
         weight: req.body.weight,
-        telephones: req.body.telephones,
-        bmi: bmi(req.body.weight, req.body.height)
+        telephones: req.body.telephones
+        
     }
     
     users.push(user);
@@ -66,88 +66,119 @@ app.post('/users/', (req, res)=>{
 })
 
 app.get('/users/', (req,res)=>{
+    hasUsers(res)
     res.status(200).send(users);
 })
 
 app.get('/users/:id', (req, res)=>{
+    hasUsers(res)
+
     const id = req.params.id;
-    if(id>0 && id < users.length){
+    if(id>=0 && id < users.length){
+        
         res.status(200).send(users[id]);
     }else{
-        res.status(400)send(`${id} no es una posición valida para el array de usuarios`);
+        res.status(400).send(`${id} no es una posición valida para el array de usuarios`);
     }
 });
 
 app.get('/users/lastname/:lastname', (req, res)=>{
-    const lastname = req.params.lastname;
-    if(validacion(lastname)==false){
-        res.status(200).send(`Página del usuario ${lastName}`);
-    }else{
-        res.sendStatus(400);
-    }
+    hasUsers(res)
+
+    const lastname = req.params.lastname.toLowerCase();
+    
+    arrayfiltered = users.filter(user=>{
+       return user.lastName.toLowerCase() == lastname 
+    })
+    
+    if (arrayfiltered.length==0)
+        res.status(404).send(`el apellido ${lastname} no existe`);
+
+    res.status(200).send(arrayfiltered);
+    
 });
 
 app.get('/users/gender/:gender', (req, res)=>{
-    const gender = req.params.gender;
-    if(validacion(gender)==false || gender == "Hombre" || gender == "Mujer"){
-        res.status(200).send(`Página del usuario ${gender}`);
-    }else{
-        res.sendStatus(400);
-    }
+    hasUsers(res)
+
+    const gender = req.params.gender.toUpperCase();
+    arrayGenderfiltered = users.filter(user=>{
+        return user.gender.toUpperCase() == gender
+     })
+     
+     if (arrayGenderfiltered.length==0)
+         res.status(404).send(`el genero ${gender} no esta definido, las opciones son 'F' o 'M'`);
+ 
+     res.status(200).send(arrayGenderfiltered);
+      
 });
 
 
-app.get('/users/telephone', (req, res)=>{
-    const telephone = req.params.telephone;
-    if(telephone>0){
-        res.status(200).send(`${id}`);
-    }else{
-        res.sendStatus(400);
-    }
+app.get('/users/telephone/telephone/', (req, res)=>{
+    
+    hasUsers(res)
+      
+    arrayUserWithTelephone = users.filter(user=>{
+        console.log(user.telephones)
+        return user.telephones != undefined && user.telephones.length>0
+
+     })
+     
+     if (arrayUserWithTelephone.length==0)
+        res.status(404).send(`Los usuarios no tienen telefonos registrados`);
+ 
+     res.status(200).send(arrayUserWithTelephone);
+
+    
 });
 
 
 app.get('/users/bmi/:id', (req, res)=>{
+    hasUsers(res)
+
     const id = req.params.id;
-    const bmi = req.params.bmi;
-    if(id>0){
-        res.status(200).send(`Indice de masa corporal del usuario ${id}: ${bmi}`);
-    }else{
-        res.sendStatus(400);
-    }
-});
+        
+    if(id>=0 && id < users.length){
+        const weight = parseFloat(users[id].weight)
+        const height = parseFloat(users[id].height)
+        const bmiResult = bmi(weight, height);
 
-app.get('/users/bmi/', (req, res)=>{
-    const id = req.params.id;
-    const bmi = req.params.bmi;
-    if(id>0){
-        res.status(200).send(`Indice de masa corporal del usuario ${id}: ${bmi}`);
-    }else{
-        res.sendStatus(400);
-    }
-});
-
-app.get('/bmi/:weight/:height', (req, res)=>{
-    const weight = req.params.weight;
-    const height = req.params.height;
-    const bmiResult = bmi(weight, height);
-    bmiResult === -1 ?
-        res.sendStatus(500)
-    :    
-        res.status(200).send(`bmi: ${bmiResult}`);
+        bmiResult == -1 ? 
+        res.status(200).send(`Error calculo bmi para ${users[id].name}`) : 
+        res.status(200).send(`El resultado del bmi para ${users[id].name} es: ${bmiResult} `)
+       
+    }else
+        res.status(400).send(`${id} no es una posición valida para el array de usuarios`);
+    
 });
 
 
+app.get('/users/bmi/bmi/', (req, res)=>{
+  hasUsers(res)
+
+
+
+});
+
+app.delete('/users/:id', (req, res)=>{
+
+    hasUsers(res)
+   
+
+});
+
+
+hasUsers = (res)=>{
+
+   if (users == undefined || users.length == 0)
+      res.status(404).send("No existen usuarios creados.");
+
+}
 
 app.listen(3000, () => {
     console.log("Servidor iniciado");
    });
 
-   function validacion(texto){
-    for(i=0; i<texto.length; i++){
-       if (numeros.indexOf(texto.charAt(i),0)!=-1){
-          return true;
-       }
-    }
-    return false;
- }
+   
+
+   
